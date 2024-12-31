ee871aec8cf06d4a9dfa482f16adc5a16c2d4771
Praveen8104 <praveenreddygoli8@gmail.com>
2024-12-31 11:02:42 +0530

    tables

---
+++ Components/tables.js
@@ -0,0 +1,62 @@
+import { Table, Row, Rows } from "react-native-table-component";
+import { View, ScrollView, StyleSheet } from "react-native";
+
+function Tables() {
+
+    const data = {
+        tableHead: ['Name', 'Roll', 'Mobile', 'Branch', 'College'],
+        tableData: [
+            ['Praveen Reddy', '22MH1A4220', '9876543210', 'AI&ML', 'ACET'],
+            ['Sricharan', '22MH1A4223', '9876543210', 'CSE', 'ACET'],
+            ['Charan Raju', '22MH1A4250', '9876543210', 'AI&ML', 'ACET'],
+            ['Karthik', '22MH1A4254', '9876543210', 'ECE', 'ACET'],
+        ],
+    }
+
+
+    return (
+        <>
+            <ScrollView horizontal={true}>
+                <View style={styles.container}>
+                    <Table borderStyle={{ borderWidth: 1, borderColor: 'black' }}>
+                        <Row data={data.tableHead}
+                            widthArr={[130, 120, 120, 120, 120]}
+                            style={styles.head}
+                            textStyle={styles.inside_text_1} />
+                        <Rows data={data.tableData}
+                            widthArr={[130, 120, 120, 120, 120]}
+                            style={styles.data}
+                            textStyle={styles.inside_text_2} />
+                    </Table>
+                </View>
+            </ScrollView>
+        </>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 16,
+        backgroundColor: '#fff',
+        alignSelf: 'center'
+    },
+    head: {
+        height: 40,
+        backgroundColor: '#f1f8ff'
+    },
+    data: {
+        height: 40,
+    },
+    inside_text_1: {
+        fontSize: 16,
+        textAlign: 'center',
+        fontWeight: 'bold'
+    },
+    inside_text_2: {
+        fontSize: 14,
+        textAlign: 'center'
+    }
+})
+
+export default Tables;
--- index.js
+++ index.js
@@ -2,8 +2,13 @@ import { registerRootComponent } from 'expo';
 
 import App from './App';
 import Card from './Components/task';
+import Tables from './Components/tables';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
-registerRootComponent(Card);
+// registerRootComponent(Card);
+// registerRootComponent(App);
+registerRootComponent(Tables);
+
+
